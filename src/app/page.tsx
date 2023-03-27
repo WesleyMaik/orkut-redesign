1e793acbb17bf0216c78c839e2b3f01edf2d609c
WesleyMaik <wesleynamikaze404@gmail.com>
2023-03-27 17:49:12 -0400

    feat add initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+'use client';
+
+import { Login } from "@/components/Forms/Login";
+import { Button, Stack } from "@chakra-ui/react";
+
 export default function Home() {
-  return (<></>)
+  return (
+    <Stack>
+      <Login />
+    </Stack>
+  );
 };
