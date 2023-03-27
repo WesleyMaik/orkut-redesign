366345984639bb4a4fc55e9197baa6556ca2f24d
WesleyMaik <wesleynamikaze404@gmail.com>
2023-03-27 17:48:29 -0400

    feat adding custom colors in ChakraUI theme

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,4 +1,4 @@
-import { ChakraTheme, extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { ChakraTheme, defineStyleConfig, extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
     initialColorMode: "light",
@@ -10,7 +10,38 @@ const fonts: ChakraTheme['fonts'] = {
     heading: 'Roboto, Inter, system-ui, sans-serif,'
 };
 
+const colors: ChakraTheme['colors'] = {
+    primary: '#ED2590',
+    "primary.dark": "#c91877",
+    background: '#D9E6F8',
+    custom_black: '#4B4B4B',
+    custom_gray: '#E3EAF2',
+    custom_yello: '#FACF42',
+    custom_blue: '#26CAFF',
+    custom_red: '#E83838'
+};
+
+const Button = defineStyleConfig({
+    variants: {
+        primary: {
+            color: 'white',
+            backgroundColor: 'primary',
+            rounded: 999,
+            padding: 6,
+            _hover: {
+                backgroundColor: 'primary.dark',
+            }
+        }
+    }
+});
+
+const components: ChakraTheme['components'] = {
+    Button
+};
+
 export const theme = extendTheme({
     config,
-    fonts
+    fonts,
+    colors,
+    components
 });
